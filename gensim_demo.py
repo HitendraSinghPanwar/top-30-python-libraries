85250a3a2fb4e372accb898d196387fc7d9c4677
HitendraSinghPanwar <hitendrasingh0305@gmail.com>
2025-07-23 11:48:29 +0530

    Updated gensim_demo.py

--- gensim_demo.py
+++ gensim_demo.py
@@ -1,37 +1,23 @@
 import gensim
 import os
 from gensim.utils import simple_preprocess
-from gensim.models import Word2Vec, TfidfModel, LdaModel
-from gensim.corpora import Dictionary
-from gensim.similarities import MatrixSimilarity
+from gensim import corpora
+from gensim.test.utils import get_tmpfile
 
-doc = open(r"C:\Users\thaku\Downloads\archive (1)\News_Category_Dataset_v3.json", encoding="utf-8")
-
-tokenized = []
+doc = open(r"C:\Users\thaku\Downloads\archive (1)\News_Category_Dataset_v3.json", encoding ='utf-8')
+tokenized =[]
 for sentence in doc.read().split('.'):
-    tokenized.append(simple_preprocess(sentence, deacc=True))
+  tokenized.append(simple_preprocess(sentence, deacc = True))
+print(tokenized)
 
+my_dictionary = corpora.Dictionary(tokenized)
 
-w2v_model = Word2Vec(sentences=tokenized, vector_size=100, window=5, min_count=2, workers=4)
-print("\n Similar Words to 'news':")
-print(w2v_model.wv.most_similar("news", topn=5))
+my_dictionary.save('my_dictionary.dict')
+load_dict = corpora.Dictionary.load('my_dictionary.dict')
 
+tmp_fname = get_tmpfile("dictionary")
+my_dictionary.save_as_text(tmp_fname)
+load_text = corpora.Dictionary.load_from_text(tmp_fname)
 
-dictionary = Dictionary(tokenized)
-corpus = [dictionary.doc2bow(text) for text in tokenized]
-tfidf_model = TfidfModel(corpus)
-print("\nTop TF-IDF words from 1st doc:")
-for word_id, score in sorted(tfidf_model[corpus[0]], key=lambda x: -x[1])[:5]:
-    print(dictionary[word_id], round(score, 4))
-
-lda_model = LdaModel(corpus=corpus, id2word=dictionary, num_topics=3, passes=10)
-print("\nLDA Topics:")
-for idx, topic in lda_model.print_topics():
-    print(f"Topic {idx}: {topic}")
-
-
-similarity_index = MatrixSimilarity(tfidf_model[corpus])
-sim_scores = list(enumerate(similarity_index[tfidf_model[corpus[0]]]))
-print("\nSimilarity of Doc[0] with others:")
-for doc_id, score in sorted(sim_scores[1:6], key=lambda x: -x[1]):
-    print(f"Doc {doc_id} similarity: {round(score, 3)}")
+BoW_corpus =[my_dictionary.doc2bow(doc, allow_update = True) for doc in tokenized]
+print(BoW_corpus)
